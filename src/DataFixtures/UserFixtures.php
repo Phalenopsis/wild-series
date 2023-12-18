78158b58f125309958f462583cd96c4bc93a96a0
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-18 08:26:49 +0100

    logout ok

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+    private const USERS = [
+        ['email' => 'user@site.com', 'password' => 'plopuser', 'roles' => 'USER'],
+        ['email' => 'admin@site.com', 'password' => 'plopadmin', 'roles' => 'ADMIN'],
+    ];
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher){
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        foreach (self::USERS as $user){
+            $newUser = new User();
+            $newUser->setEmail($user['email']);
+            $newUser->setRoles([$user['roles']]);
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $newUser,
+                $user['password'],
+            );
+            $newUser->setPassword($hashedPassword);
+            $manager->persist($newUser);
+        }
+
+        $manager->flush();
+    }
+}

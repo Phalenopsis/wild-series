a6b88573481cb81cd5135db960ac11310aced30a
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-26 19:24:11 +0100

    add is granted admin to create a category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,6 +11,7 @@ use App\Form\CategoryType;
 use App\Entity\Category;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
@@ -26,6 +27,7 @@ class CategoryController extends AbstractController
     }
 
     #[Route('/new', name: 'new')]
+    #[IsGranted('ROLE_ADMIN')]
     public function new(Request $request, EntityManagerInterface $entityManager) : Response
     {
         $category = new Category();

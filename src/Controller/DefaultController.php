f38c7007a123a903d81ca15a17214bc258bf57e5
Nicolas Leroux <hertford.fr@gmail.com>
2023-12-27 18:44:28 +0100

    add route to profil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,19 +3,30 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
+        $lastPrograms = $programRepository->findBy([], ['id' => 'DESC'], 5);
+
         $categories = $categoryRepository->findAll();
         return $this->render('index.html.twig',
         [
             'categories' => $categories,
+            'last_programs' => $lastPrograms,
         ]);
+    }
+
+    #[Route('/my-profile', name: 'app_profil')]
+    public function showProfil(): Response
+    {
+        return $this->render('home/index.html.twig');
     }
 }

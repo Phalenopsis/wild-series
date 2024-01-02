f01470593062fd1412506cf705e4fd6966eee34a
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-02 09:32:38 +0100

    add live component to search a program

--- src/Twig/Components/ProgramSearch.php
+++ src/Twig/Components/ProgramSearch.php
@@ -2,11 +2,25 @@
 
 namespace App\Twig\Components;
 
+use App\Repository\ProgramRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 
 #[AsLiveComponent()]
 final class ProgramSearch
 {
     use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    public function __construct(private ProgramRepository $programRepository){
+
+    }
+
+    public function getPrograms(): array
+    {
+        return $this->programRepository->findLikeName($this->query);
+    }
 }
